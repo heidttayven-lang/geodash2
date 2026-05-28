4f8d4a92dc6b64f08485d036d6a2e7a067b0a317
heidttayven-lang <heidt.tayven@prairiesouth.ca>
2026-05-28 11:23:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let keys = {};
+let speed = 6;
+
+document.addEventListener("keydown", e => keys[e.code] = true);
+document.addEventListener("keyup", e => keys[e.code] = false);
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  updatePlayer();
+  updateLevel();
+  updatePortals();
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
