75962fad4dad24963513b7d2bc18a4d9290b6888
heidttayven-lang <heidt.tayven@prairiesouth.ca>
2026-05-28 11:33:54 -0600

    Update editor.js

--- js/editor.js
+++ js/editor.js
@@ -1,19 +1,23 @@
-let editorMode = false;
+const canvas = document.getElementById("game");
 
-document.addEventListener("keydown", e => {
-  if (e.code === "KeyE") {
-    editorMode = !editorMode;
+if (!canvas) {
+  throw new Error("Canvas not found in editor.js");
+}
+
+const ctx = canvas.getContext("2d");
+
+// Example editor state
+let objects = [];
+
+function addObject(x, y) {
+  objects.push({ x, y });
+}
+
+// Example draw loop for editor tools
+function drawEditor() {
+  ctx.fillStyle = "white";
+
+  for (let obj of objects) {
+    ctx.fillRect(obj.x, obj.y, 10, 10);
   }
-});
-
-canvas.addEventListener("click", e => {
-  if (!editorMode) return;
-
-  objects.push({
-    x: e.clientX,
-    y: e.clientY,
-    w: 40,
-    h: 40,
-    type: "spike"
-  });
-});
+}
